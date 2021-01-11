0aa2d908b6ed65b50197dd583aa37dbc774c9833
HristoDikov? <dikoww7@gmail.com>
2021-01-11 23:57:25 +0200

    Extract Startup.cs logic

---
+++ Cinema.Server/Infrastructure/ConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Cinema.Server.Infrastructure
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ConfigurationExtensions
+    {
+        public static string GetDefaultConnectionString(this IConfiguration configuration)
+        => configuration.GetConnectionString("DefaultConnection");
+
+        public static ApplicationSettings GetApplicationSettings(this IServiceCollection services, IConfiguration configuration) 
+        {
+            var applicationSettingConfig = configuration.GetSection("ApplicationSettings");
+            services.Configure<ApplicationSettings>(applicationSettingConfig);
+
+            var appSettings = applicationSettingConfig.Get<ApplicationSettings>();
+
+            return appSettings;
+        }
+    }
+}
